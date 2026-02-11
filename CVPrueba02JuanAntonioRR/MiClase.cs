3f9d3d5a8499035ef6c8aa05cfa87a2831c5c9d6
jrrbachillerato <jrrbachillerato@gmail.com>
2026-02-11 09:06:25 +0100

    Estructura de una Clase
    Diferentes secciones de la clase con una breve descripción de su funcionalidad.

--- CVPrueba02JuanAntonioRR/MiClase.cs
+++ CVPrueba02JuanAntonioRR/MiClase.cs
@@ -8,5 +8,17 @@ namespace CVPrueba02JuanAntonioRR
 {
     public class MiClase
     {
+        // CONSTANTES - Aspectos de configuración de la clase.
+
+        // CAMPOS/MIEMBROS PRIVADOS - Agrupar Datos Protegidos (No accesibles desde fuera de la clase.)
+        // NOMENCLATURA: _nombreCampo
+
+        // CONSTRUCTORES - Métodos especiales que definen la forma de instanciar los objetos de la clase. 
+        // Si no se definen constructores la clase facilita uno por defecto. - MiClase()
+
+        // PROPIEDADES - Interfaz que ofrece la clase para controlar el acceso a los campos protegidos.
+        // Implementa los métodos GET y SET para establecer los "datos asociados".
+        
+        // MÉTODOS PÚBLICOS Y PRIVADOS        
     }
 }
