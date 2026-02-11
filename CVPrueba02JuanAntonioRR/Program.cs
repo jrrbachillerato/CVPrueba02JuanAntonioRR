4b54ceeb603453fa00f17428a9c1cb057f06ccca
jruir <jruir@DAM1-PC3>
2026-02-11 08:31:01 +0100

    Agregar archivos de proyecto.

---
+++ CVPrueba02JuanAntonioRR/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CVPrueba02JuanAntonioRR
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
